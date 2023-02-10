7457101c486307b5218034c6b15582ecc5904fbd
Алексей Чайкин <barjastroy@gmail.com>
2023-02-10 21:45:40 +0300

    Update HeaderDetailView.swift

--- surf_test/HeaderDetailView.swift
+++ surf_test/HeaderDetailView.swift
@@ -18,6 +18,21 @@ class HeaderDetailView: UIView {
         
         return label
     }()
+    
+    let desc : UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.text = "Работай над реальными задачами под руководством опытного наставника и получи возможность стать частью команды мечты. "
+        label.font = UIFont(name: "SFProDisplay-Regular", size: 16)
+        label.adjustsFontSizeToFitWidth = true
+        label.sizeToFit()
+        label.textAlignment = .natural
+        label.textColor = #colorLiteral(red: 0.5882352941, green: 0.5843137255, blue: 0.6078431373, alpha: 1)
+        
+        return label
+    }()
 
     init() {
         super.init(frame: .zero)
@@ -25,6 +40,7 @@ class HeaderDetailView: UIView {
         self.backgroundColor = #colorLiteral(red: 0.9999127984, green: 1, blue: 0.9998814464, alpha: 1)
         
         self.addSubview(title)
+        self.addSubview(desc)
         
         setup()
         
@@ -36,6 +52,15 @@ class HeaderDetailView: UIView {
     
     func setup() {
         // setup title
-//        title
+        title.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        title.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        title.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        title.heightAnchor.constraint(equalToConstant: 32).isActive = true
+        
+        // setup description
+        desc.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 12).isActive = true
+        desc.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        desc.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+//        desc.heightAnchor.constraint(equalToConstant: 32).isActive = true
     }
 }
