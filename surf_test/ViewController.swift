48789be52ab5d49ead64ab7cdc748f0a4ec82dd5
Алексей Чайкин <barjastroy@gmail.com>
2023-02-10 14:30:15 +0300

    add mainPicture

--- surf_test/ViewController.swift
+++ surf_test/ViewController.swift
@@ -9,13 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let mainImageView : UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "image2")
+        view.backgroundColor = .red
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.contentMode = .scaleAspectFill
+//        view.contentMode = .scaleToFill
+//        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(mainImageView)
+        setupMainImageView()
         
     }
     
-    func start() {
-        
+    func setupMainImageView() {
+        mainImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        mainImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        mainImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        mainImageView.heightAnchor.constraint(equalToConstant: 500).isActive = true
     }
 
 
