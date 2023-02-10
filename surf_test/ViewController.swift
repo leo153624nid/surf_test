d47b57603e636dbc17ec499ed5af56ae43b1c5af
Алексей Чайкин <barjastroy@gmail.com>
2023-02-10 16:51:02 +0300

    Update ViewController.swift

--- surf_test/ViewController.swift
+++ surf_test/ViewController.swift
@@ -9,29 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let mainImageView : UIImageView = {
-        let view = UIImageView()
-        view.image = UIImage(named: "image2")
-        view.backgroundColor = .red
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.contentMode = .scaleAspectFill
-//        view.contentMode = .scaleToFill
-//        view.contentMode = .scaleAspectFit
-        return view
-    }()
+   let mainImageView = MainImageView()
+    let detailView = DetailView(width: 100, height: 100)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.addSubview(mainImageView)
-        setupMainImageView()
+        view.addSubview(detailView)
+        setupMainView()
         
     }
     
-    func setupMainImageView() {
+    func setupMainView() {
+        // setup MainImageView
         mainImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         mainImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         mainImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        mainImageView.heightAnchor.constraint(equalToConstant: 500).isActive = true
+        mainImageView.heightAnchor.constraint(equalToConstant: mainImageView.bounds.height).isActive = true
+        print(mainImageView.bounds.height)
+        
+        // setup DetailView
+        detailView.height = 200
     }
 
 
