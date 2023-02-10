86a62a33d348647ff0ee7ab72556fdd6a8993535
Алексей Чайкин <barjastroy@gmail.com>
2023-02-10 16:50:59 +0300

    Create MainImageView.swift

---
+++ surf_test/MainImageView.swift
@@ -0,0 +1,24 @@
+//
+//  MainImageView.swift
+//  surf_test
+//
+//  Created by macbook on 10.02.2023.
+//
+
+import UIKit
+
+class MainImageView: UIImageView {
+
+    let mainImage = UIImage(named: "image3")
+    
+    init() {
+        super.init(image: mainImage)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.contentMode = .scaleAspectFill
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
