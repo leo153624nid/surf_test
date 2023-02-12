15c4bbda4b64db79d33a500bcd8b9b9c233b4c38
Алексей Чайкин <barjastroy@gmail.com>
2023-02-12 20:14:54 +0300

    Update Model.swift

--- surf_test/Model.swift
+++ surf_test/Model.swift
@@ -12,16 +12,6 @@ struct CarouselItem {
     var button: CustomButton
     
     static func fetchCarouselData() -> [CarouselItem] {
-//        let one = CarouselItem(button: CustomButton(title: "iOS", id: 1))
-//        let two = CarouselItem(button: CustomButton(title: "Android", id: 2))
-//        let three = CarouselItem(button: CustomButton(title: "Design", id: 3))
-//        let four = CarouselItem(button: CustomButton(title: "Flutter", id: 4))
-//        let five = CarouselItem(button: CustomButton(title: "QA", id: 5))
-//        let six = CarouselItem(button: CustomButton(title: "PM", id: 6))
-//        let seven = CarouselItem(button: CustomButton(title: "JS", id: 7))
-//        let eight = CarouselItem(button: CustomButton(title: "React", id: 8))
-//        let nine = CarouselItem(button: CustomButton(title: "HTML", id: 9))
-//        let ten = CarouselItem(button: CustomButton(title: "CSS", id: 10))
         let one = CarouselItem(button: CustomButton(title: "iOS"))
         let two = CarouselItem(button: CustomButton(title: "Android"))
         let three = CarouselItem(button: CustomButton(title: "Design"))
@@ -40,21 +30,19 @@ class CustomButton: UIButton, Identifiable {
     var id = UUID()
     var buttonIsOn = false {
         didSet {
-            self.backgroundColor = buttonIsOn ? #colorLiteral(red: 0.1921568627, green: 0.1921568627, blue: 0.1921568627, alpha: 1) : #colorLiteral(red: 0.9529411765, green: 0.9529411765, blue: 0.9607843137, alpha: 1)
-            self.setTitleColor(buttonIsOn ? #colorLiteral(red: 0.9529411765, green: 0.9529411765, blue: 0.9607843137, alpha: 1) : #colorLiteral(red: 0.1921568627, green: 0.1921568627, blue: 0.1921568627, alpha: 1), for: .normal)
+            self.backgroundColor = buttonIsOn ? Constants.blackColor : Constants.greyColor
+            self.setTitleColor(buttonIsOn ? Constants.greyColor : Constants.blackColor, for: .normal)
         }
     }
 
     init(title: String) {
-//        self.id = id
-        super.init(frame: .zero)
-        
+        super.init(frame: .zero)    
         self.translatesAutoresizingMaskIntoConstraints = false
-        self.backgroundColor = buttonIsOn ? #colorLiteral(red: 0.1921568627, green: 0.1921568627, blue: 0.1921568627, alpha: 1) : #colorLiteral(red: 0.9529411765, green: 0.9529411765, blue: 0.9607843137, alpha: 1)
+        self.backgroundColor = Constants.greyColor
         self.layer.cornerRadius = 12
         self.titleLabel?.font = UIFont(name: "SFProDisplay-Regular", size: 16)
-        self.setTitleColor(buttonIsOn ? #colorLiteral(red: 0.9529411765, green: 0.9529411765, blue: 0.9607843137, alpha: 1) : #colorLiteral(red: 0.1921568627, green: 0.1921568627, blue: 0.1921568627, alpha: 1), for: .normal)
-        self.setTitleColor(#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), for: .highlighted)
+        self.setTitleColor(Constants.blackColor, for: .normal)
+        self.setTitleColor(Constants.yellowColor, for: .highlighted)
         self.setTitle(title, for: .normal)
         
     }
@@ -65,6 +53,11 @@ class CustomButton: UIButton, Identifiable {
 }
 
 struct Constants {
+    static let whiteColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    static let yellowColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+    static let greyColor = #colorLiteral(red: 0.9529411765, green: 0.9529411765, blue: 0.9607843137, alpha: 1)
+    static let blackColor = #colorLiteral(red: 0.1921568627, green: 0.1921568627, blue: 0.1921568627, alpha: 1)
     static let one : CGFloat = 10 // todo
+    
 }
 
